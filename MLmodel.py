24e3cc873bfc8901d60722e5b0a57eae066e5bb7
KIRUBA KARUPPANNAN <61502293+kirubamay20@users.noreply.github.com>
2020-11-24 21:50:17 +0530

    Update MLmodel.py

--- MLmodel.py
+++ MLmodel.py
@@ -1,9 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
-wine= load_breast_cancer()
+wine= load_diabetes()
 X=wine.data
 y=wine.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.5)
