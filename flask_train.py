04f2b2601cbac93adaf27516a48b0f8c2e722ba5
KIRUBA KARUPPANNAN <61502293+kirubamay20@users.noreply.github.com>
2020-11-24 20:09:22 +0530

    Create flask_train.py

---
+++ flask_train.py
@@ -0,0 +1,20 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+wine= load_wine()
+X=wine.data
+y=wine.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.5)
+clf=RandomForestClassifier(n_estimators=10)
+clf.fit(X_train, y_train)
+predicted=clf.predict(X_test)
+print(accuracy_score(predicted,y_test))
+
+import pickle 
+from flask import Flask
+import numpy as np
+with open('/content/sample_data/breast_cancer_rf.pkl', 'wb') as model_file:
+  model=pickle.load(model_file)
+
